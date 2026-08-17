ca3dfbcf7074166eeafc41bc5f71ca12e939a9fd
hoanglong9393-stack <hoanglong9393@gmail.com>
2026-08-17 11:14:25 +0700

    Update game_pnl_dragdrop.py

--- game_pnl_dragdrop.py
+++ game_pnl_dragdrop.py
@@ -7,7 +7,7 @@ import io
 import json
 import requests
 
-st.set_page_config(page_title="Game P&L Forecast Pro - Hoàng Thành Long", layout="wide", page_icon="🎮")
+st.set_page_config(page_title="P&L Forecast Tool - Hoàng Thành Long", layout="wide", page_icon="🎮")
 
 GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby9Vn-O9aaUjikdUMUnb5h063WCAlVnFVK2SwcIGYSRxj3qeoB8h1-T909WO3KtVWl9sw/exec"
 
